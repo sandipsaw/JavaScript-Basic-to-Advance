01d2265b44ae47653a60dc12f51984e943aee451
sandip saw <sandipsaw449@gmail.com>
2025-10-31 21:17:18 +0530

    Basic question on for loop

---
+++ For-Loop/Basic.js
@@ -0,0 +1,114 @@
+let prompt = require('prompt-sync')();
+//1. Print numbers from 1 to 10.
+
+// for(let i=1;i<=10;i++){
+//     console.log(i);
+// }
+
+//2. Print numbers from 10 to 1.
+
+// for(let i=10; i>=1;i--){
+//     console.log(i);
+// }
+
+//3. Print even numbers between 1 and 20.
+
+// for(let i=1;i<=20;i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+
+//4. Print odd numbers between 1 and 20.
+
+// for(let i=1;i<=20;i++){
+//     if(i%2!=0){
+//         console.log(i);
+//     }
+// }
+
+//5. Print all numbers from 1 to 50 that are divisible by 5.
+
+// for(let i=1;i<=50;i++){
+//     if(i%5==0){
+//         console.log(i);
+//     }
+// }
+
+//6. Find the sum of first 10 natural numbers.
+
+// let sum=0;
+// for(let i=1;i<=10;i++){
+//     sum += i
+// }
+// console.log(sum);
+
+//7. Find the sum of all even numbers from 1 to 50.
+
+// let sum =0;
+// for(let i=1;i<=50;i++){
+//     if(i%2==0){
+//         sum += i
+//     }
+// }
+// console.log(sum);
+
+//8. Find the sum of all odd numbers from 1 to 50.
+
+// let sum =0;
+// for(let i=1;i<=50;i++){
+//     if(i%2!=0){
+//         sum += i
+//     }
+// }
+// console.log(sum);
+
+//9. Print the multiplication table of 7.
+
+// let n = Number(prompt('enter a number : '));
+// for(let i=1 ; i<=10;i++){
+//     console.log(i*n);
+// }
+
+//10. Print the squares of numbers from 1 to 10.
+
+// for(let i=1;i<=10;i++){
+//     let square = Math.pow(i,2);
+//     console.log(square);
+// }
+
+//11. Print the cubes of numbers from 1 to 10.
+
+// for(let i=1;i<=10;i++){
+//     let square = Math.pow(i,3);
+//     console.log(square);
+// }
+
+//12. Print all numbers from 1 to 100, but skip numbers divisible by 3.
+
+// for(let i=1;i<=100;i++){
+//     if(i%3!=0){
+//         console.log(i);
+//     }
+// }
+
+//13. Print numbers from 1 to 20, but stop the loop when you reach 13.
+
+// for(let i=1;i<=20;i++){
+//     if(i ==13) continue;
+//     console.log(i);
+// }
+
+//14. Print all elements of an array [5, 10, 15, 20, 25].
+
+// let array = [5, 10, 15, 20, 25];
+// for(let i=0;i<array.length;i++){
+//     console.log(array[i]);
+// }
+
+//15. Print elements of an array in reverse order.
+
+// let array = [5, 10, 15, 20, 25];
+// for(let i=array.length;i>=0;i--){
+//     console.log(array[i]);
+// }
